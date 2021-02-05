09ac5690d71eea49dc09a03174591f4188d50111
goofyweng <peter072627@gmail.com>
2021-02-05 23:52:15 +0800

    commit ex8-1

--- 2/LAB8/ex8-1.cpp
+++ 2/LAB8/ex8-1.cpp
@@ -1,10 +1,7 @@
 //ex8-1.cpp
 //type includes student id, gender, age,
 //and the amount of cash a student owns.
-//"Youth" ==> age of one student is < 21
-//"Adult" ==> the student is >= 21 years old.
-// "Rich" ==> money is >=$10000
-//"poor" ==> money is < $10000
+
 #include <iostream>
 #include <cstring>
 using namespace std;
@@ -17,24 +14,74 @@ struct student{
 };
 
 void ShowData(const student& );
+student SetStudent();
+void Caculate(const student&, const student&, const student&, const student&, const student&);
 int main()
 {
   student a = {9413599, 'M', 25, 10001};
   student b = {9513622, 'M', 22, 500};
   student c = {9613812, 'F', 20, 30};
   student d = {9713964, 'F', 18, 66000};
+  student e = {0, 0, 0, 0};
+  e = SetStudent();
   ShowData(a);
+  ShowData(b);
+  ShowData(c);
+  ShowData(d);
+  ShowData(e);
+  Caculate(a, b, c, d, e);
   return 0;
+}
+
+//"Youth" ==> age of one student is < 21
+//"Adult" ==> the student is >= 21 years old.
+// "Rich" ==> money is >=$10000
+//"poor" ==> money is < $10000
+student SetStudent()
+{
+  student s;
+  cout << "Please enter your student id: ";
+  cin >> s.id;
+  cout << "Gender (M/F): ";
+  cin >> s.gender;
+  cout << "Age: ";
+  cin >> s.age;
+  cout << "The amount of money in your own: ";
+  cin >> s.money;
+  return s;
 }
 
 void ShowData(const student& a)
 {
-  if (a.age >= 21)
-    {char AorY[6] = "Adult";
-    cout << AorY;}
-  else
-    char AorY[6] = "Youth";
+  char AorY[10];
+  char RorP[10];
+  char MorF[10];
 
-  // cout << "id " << a.id << " is a " << AorY[6]
-  //   << " & " << a.money << " " << a.gender << "!" << endl;
+  if (a.age < 21)
+    strcpy(AorY, "youth");
+  else
+    strcpy(AorY, "adult");
+
+  if (a.money >= 10000)
+    strcpy(RorP, "rich");
+  else
+    strcpy(RorP, "poor");
+
+  if (a.gender == 'M')
+    strcpy(MorF, "Male");
+  else
+    strcpy(MorF, "Female");
+
+  cout << "id " << a.id << " is a " << AorY
+    << " & " << RorP << " " << MorF << "!" << endl;
+}
+
+void Caculate(const student& a, const student& b, const student& c, const student& d, const student& e)
+{
+  float AvAge;
+  int TotMon;
+  AvAge = float(a.age + b.age + c.age + d.age + e.age)/5;
+  TotMon = (a.money + b.money + c.money + d.money + e.money);
+  cout << "This group is " << AvAge << " years old in average." << endl;
+  cout << "This group has $" << TotMon << " now." << endl;
 }
